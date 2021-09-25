13b68dcc06cc0b424468329a023ce0d78d0d0706
Evgeniy Evdokimov <evdokimov.ek@gmail.com>
2021-09-25 06:30:36 +0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by 19336088 on 25.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
